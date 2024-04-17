8733d299f3255bee447abf64aa5067e4ef0ef57f
Vikas Kumar <89458526+Vika1402@users.noreply.github.com>
2024-04-17 06:04:50 +0530

    port fix

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ;
 
 //run listen
 app.listen(PORT, () => {
