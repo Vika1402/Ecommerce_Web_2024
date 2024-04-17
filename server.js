f1fd79e84172dae047d6dd34986bb4d63ffcda72
Vikas Kumar <89458526+Vika1402@users.noreply.github.com>
2024-04-17 15:09:30 +0530

    ok

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
-import {fileURLToPath} from 'url'
+import path from "path";
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
 
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,13 +32,12 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use('*',function(req,res){
-res.sendFile(path.join(__dirname,'./client/build/index.html'))
-
-})
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //PORT
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
