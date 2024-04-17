ebb53d8ff0ae3105ce2491dfba918da8e85708b8
Vikas Kumar <89458526+Vika1402@users.noreply.github.com>
2024-04-17 15:14:37 +0530

    okok

--- server.js
+++ server.js
@@ -7,43 +7,46 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Configure environment variables
+import path from 'path'
+import {fileURLToPath} from 'url'
+//configure env
 dotenv.config();
 
-// Database configuration
+//databse config
 connectDB();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Rest object
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+//rest object
 const app = express();
 
-// Middleware
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname,"./client/build")))
 
-// Routes
+
+//routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// Wildcard route to serve the React app
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+//rest api
+app.use('*',function(req,res,next){
+res.sendFile(path.join(__dirname,'./client/build/index.html'))
+  // Middleware logic
+  next();
 
-// PORT
-const PORT = process.env.PORT || 8080;
+})
 
-// Start the server
+//PORT
+const PORT = process.env.PORT ;
+
+//run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
   );
 });
